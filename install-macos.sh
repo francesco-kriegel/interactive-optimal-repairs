a1ec41179de81c9351720ac84f720a5b893ccdd1
Francesco Kriegel <francesco-kriegel@users.noreply.github.com>
2024-01-30 13:06:33 +0100

    Modified installer script to make it independent of the latest GraalVM version

--- install-macos.sh
+++ install-macos.sh
@@ -5,8 +5,8 @@ rm Protege-5.6.3-platform-independent.zip
 curl -L https://download.oracle.com/graalvm/21/latest/graalvm-jdk-21_macos-x64_bin.tar.gz --output graalvm-jdk-21_macos-x64_bin.tar.gz
 tar -xvzf graalvm-jdk-21_macos-x64_bin.tar.gz
 rm graalvm-jdk-21_macos-x64_bin.tar.gz
-mv graalvm-jdk-21.0.1+12.1/Contents/Home Protege-5.6.3/jre
-rm -rf graalvm-jdk-21.0.1+12.1
+mv $(ls -1 | grep "graalvm-jdk")/Contents/Home Protege-5.6.3/jre
+rm -rf graalvm-jdk*
 
 curl -L https://github.com/francesco-kriegel/interactive-optimal-repairs/releases/download/v0.1.4/interactive-optimal-repairs-0.1.4.jar --output Protege-5.6.3/plugins/interactive-optimal-repairs-0.1.4.jar
 
